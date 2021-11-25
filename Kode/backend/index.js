209330d5601014cb85eb0513b0e0e05f30c4df41
Sander Lysakermoen <slysakermoen@gmail.com>
2021-11-25 09:00:30 +0100

    /getElements og /createElements
    Laget endpointene /getElements og /createElements, men ikke koblet til databasen.

--- Kode/backend/index.js
+++ Kode/backend/index.js
@@ -4,7 +4,7 @@ const bp = require('body-parser')
 const cors = require('cors')
 const mysql = require('mysql')
 const JSON = require('JSON')
-const port = 8000
+const port = 500
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -17,9 +17,9 @@ let connection = mysql.createConnection({
 	database: 'geirOppgave'
 })
 
-connection.connect(function(err) {
+/*connection.connect(function(err) {
 	console.log(err)
-})
+})*/
 
 function createTable(name) {
 	connection.query('CREATE TABLE ' + name, function(result){
@@ -41,18 +41,36 @@ function deleteTable(name) {
 	})
 }
 
+function createElement(ID, table, title, body) {
+	console.log(`USE ${ID}`)
+	console.log(`SELECT TABLE ${table}`)
+	console.log(`INSERT INTO ${table}(Title, Body) VALUES ('${title}', '${body}');`)
+}
+
+function getElements(ID, table, /*callback*/) {
+	console.log(`USE ${ID}`)
+	console.log(`SELECT TABLE ${table}`)
+	console.log(`SELECT * FROM ${table}`/*, function(err, result) {
+		if (err) console.log(err)
+		result = `{"Hei hei": "Wow": ${table}, "table", "ID": ${ID}}`
+		return callback(result)
+	}*/)
+	result = `{"Hei hei": "Wow": ${table}, "table", "ID": ${ID}}`
+	return(result)
+}
+
 app.get('/getTable', function(req, res) {
 	table = req.headers.table
-	result = getTable(table, function(result) {
-		response = {
+	getTable(table, function(result) {
+		/*response = {
 			"Field": result[0].Field,
 			"Type": result[0].Type,
 			"Null": result[0].Null,
 			"Key": result[0].Key,
 			"Default": result[0].Default,
 			"Extra": result[0].Extra
-		}
-		response = JSON.stringify(response)
+		}*/
+		response = JSON.stringify(result)
 		console.log(response)
 		res.send(response)
 	})
@@ -73,6 +91,34 @@ app.post ('/createTable', function(req, res){
 	createTable(req.body.tittel + '(' + req.body.data + ')')
 	res.send();
 })
+
+app.post('/createElement', function(req, res) {
+	ID = req.body.ID
+	table = req.body.table
+	title = req.body.title
+	body = req.body.body
+	createElement(ID, table, title, body)
+	res.send();
+})
+
+app.get('/getElements', function(req, res) {
+	id = req.headers.id
+	table = req.headers.table
+	result = getElements(id, table/*, function(result) {*/
+		/*response = {
+			"Field": result[0].Field,
+			"Type": result[0].Type,
+			"Null": result[0].Null,
+			"Key": result[0].Key,
+			"Default": result[0].Default,
+			"Extra": result[0].Extra
+		}*/
+	)
+	response = JSON.stringify(result)
+	console.log(response)
+	res.send(response)
+	}//)
+/*}*/)
 
 app.listen(port, () => {
 	console.log(`Listening at localhost:${port}`)
